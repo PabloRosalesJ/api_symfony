3380ca63e9b846ed39c6e1716d0e357135888cec
PabloRosales <rosalesjuarezpablojesus@gmail.com>
2023-08-17 21:48:55 -0600

    Add DB

--- app/front.php
+++ app/front.php
@@ -5,6 +5,60 @@ use Symfony\Component\HttpFoundation\Request;
 use Crimsoncircle\Simplex\StringResponseListener;
 use Symfony\Component\DependencyInjection\Reference;
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+// use Illuminate\Events\Dispatcher;
+// use Illuminate\Container\Container;
+/* Eloquent */
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'db',
+    'database' => 'app',
+    'username' => 'root',
+    'password' => 'crimsoncircle',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+// $capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+/* Create table with blueprint */
+
+// $table = Capsule::schema()->create('posts', function ($table) {
+//     $table->id();
+//     $table->string('title');
+//     $table->string('content');
+//     $table->string('author');
+//     $table->string('slug')->unique();
+//     $table->timestamps();
+// });
+
+// $table = Capsule::schema()->create('comments', function ($table) {
+//     $table->id();
+//     $table->unsignedBigInteger('post_id');
+//     $table->string('content');
+//     $table->string('author');
+//     $table->timestamps();
+
+//     $table->foreign('post_id')->references('id')->on('posts');
+// });
+
+/* END Create table with blueprint */
+
+/*  */
+
+/* Eloquent */
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/src/app.php';
 
